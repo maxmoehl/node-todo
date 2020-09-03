0c77e254b73f5fba0878da81323ac33dfe7a2c09
Maximilian Moehl <maximilian.moehl@sap.com>
2020-09-03 14:22:37 +0200

    lower coverage requirement to 90%

--- .github/scripts/check-coverage.sh
+++ .github/scripts/check-coverage.sh
@@ -2,6 +2,7 @@
 
 COVERAGE=$(jq .total.statements.pct coverage/coverage-summary.json)
 
-if [ "$(echo "${COVERAGE} < 95.0" | bc)" -eq 1 ]; then
+if [ "$(echo "${COVERAGE} < 90.0" | bc)" -eq 1 ]; then
+  echo "Statement coverage for tests should be at least 90%"
   exit 1
 fi
