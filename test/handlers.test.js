fb651be4bf29bcf9a445250b585507ff4efaddc5
Maximilian Moehl <maximilian.moehl@sap.com>
2020-09-11 10:23:49 +0200

    add more tests and fix bugs

--- test/handlers.test.js
+++ test/handlers.test.js
@@ -2,12 +2,37 @@ const axios = require('axios')
 const router = require('../lib/router');
 
 describe('#newRouter()', () => {
-    it('should post item', (async () => {
+    it('should post todo', (async () => {
+        let server = router.newRouter().listen(8080);
+        let resp = await axios.post('http://localhost:8080/todo',
+            {
+                'title': 'Test1',
+                'description': 'Test Description'
+            });
+        if (resp.status !== 200) {
+            throw 'received non 200 status code ' + resp.status;
+        }
+        server.close();
+    }));
+    it('should get todos', async () => {
         let server = router.newRouter().listen(8080);
         let resp = await axios.get('http://localhost:8080/todos');
         if (resp.status !== 200) {
             throw 'received non 200 status code ' + resp.status;
         }
         server.close();
-    }))
+    });
+    it('should get specific todo', async () => {
+        let server = router.newRouter().listen(8080);
+        let resp = await axios.get('http://localhost:8080/todos');
+        if (resp.status !== 200) {
+            throw 'received non 200 status code ' + resp.status;
+        }
+        let todoId = resp.data.data[0].uuid;
+        resp = await axios.get('http://localhost:8080/todos/' + todoId + '/');
+        if (resp.status !== 200) {
+            throw 'received non 200 status code ' + resp.status;
+        }
+        server.close();
+    });
 })
