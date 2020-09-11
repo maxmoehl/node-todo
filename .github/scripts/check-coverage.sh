9e547cc8bcdf3b99bef2717833238ba4cccfd0ad
Maximilian Moehl <maximilian.moehl@sap.com>
2020-09-11 10:13:14 +0200

    fix coverage script

--- .github/scripts/check-coverage.sh
+++ .github/scripts/check-coverage.sh
@@ -3,6 +3,6 @@
 COVERAGE=$(jq .total.statements.pct coverage/coverage-summary.json)
 
 if [ "$(echo "${COVERAGE} < 90.0" | bc)" -eq 1 ]; then
-  echo "Statement coverage for tests should be at least 90% but is $($COVERAGE)%"
+  echo "Statement coverage for tests should be at least 90% but is $COVERAGE%"
   exit 1
 fi
